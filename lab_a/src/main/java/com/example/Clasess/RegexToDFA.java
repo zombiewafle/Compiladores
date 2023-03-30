b7c271bf449026c63149d86dfd82588b393ec865
Javier Salazar <40800436+zombiewafle@users.noreply.github.com>
2023-03-30 02:39:36 -0600

    Add files via upload

--- lab_a/src/main/java/com/example/Clasess/RegexToDFA.java
+++ lab_a/src/main/java/com/example/Clasess/RegexToDFA.java
@@ -10,18 +10,18 @@ import java.util.Stack;
 public class RegexToDFA {
     public Map<Map<Transition, Boolean>, Map<List<Integer>, List<Integer>> > registry;
     String regString;
-    List<Integer> followPos;    
-    Boolean nullable;
+    // List<Integer> followPos;    
+    // Boolean nullable;
     // List<String> firstPos;
     // List<String> lastPos;
     Integer counter;
-    private int[][] matrix;
+    // private int[][] matrix;
 
     public RegexToDFA(String regexString){
         this.registry = new HashMap<>();
         this.regString = regexString+"#";
-        this.followPos = new LinkedList<>();
-        this.nullable = false;
+        // this.followPos = new LinkedList<>();
+        // this.nullable = false;
         // this.firstPos = new LinkedList<>();
         // this.lastPos = new LinkedList<>();
         this.counter = 0;
@@ -43,34 +43,68 @@ public class RegexToDFA {
         Stack<Boolean> stack2 = new Stack<>(); //It  keeps control storing the nullability
         Stack<String> stack3 = new Stack<>(); //It'll keep the value of the leafs, to be able to get all elements in the tree
 
-        Iterator<String> iterator = null;
-        String aNode = "";
-        String bNode = "";
+        // Iterator<String> iterator = null;
+        // String currNode = "";
+        // String aNode = "";
+        // String bNode = "";
         List<Integer> firstPos = new LinkedList<>();
         List<Integer> lastPos = new LinkedList<>();
-        
-        Map<Boolean, Character> map1 = new HashMap<>();
-        Map<List<Integer>, List<Integer>> map2 = new HashMap<>();
+
         Boolean recurBooleanC1 = false;
         Boolean recurBooleanC2 = false;
+        Boolean nullable = false; //curr node
 
-        // while(iterator.hasNext()){
-        //     String current = iterator.next();
-            
-        // }
+        Stack<Integer> tempFpos = new Stack<>();
+        Stack<Integer> tempLpos = new Stack<>();
 
         for (Character c: regString.toCharArray()) {
             switch(c){
                 case '.':
                     //If the 2 are nullable, IT is nullable
                     // stack2.push(nullable);
+
+                    //if true, is nullable
                     if (stack2.size() >= 2 && stack2.get(stack2.size() - 1) && stack2.get(stack2.size() - 2)) {
                         //Both are true
                         recurBooleanC1 = true; 
                         recurBooleanC2 = true; 
 
-                        nullable = false;
+                        nullable = true;
                         stack2.add(nullable);
+
+                        //FirstPos
+                        if(recurBooleanC1 == true){
+                            int count = 0;
+                            while(!tempFpos.isEmpty() && count < 2){
+                                Integer item = tempFpos.pop();
+                                firstPos.add(item);
+                                count++;
+                            }
+                        }
+
+                        else{
+                            if (tempFpos.size() >= 3) {
+                                int antepenultimate = tempFpos.get(tempFpos.size() - 3);
+                                firstPos.add(antepenultimate);
+                            }                            
+                        }
+                    
+                        //LastPos
+                        if(recurBooleanC2 == true){
+                            int count = 0;
+                            while(!tempLpos.isEmpty() && count < 2){
+                                Integer item = tempLpos.pop();
+                                lastPos.add(item);
+                                count++;
+                            }
+                        }
+
+                        else{
+                            if (tempLpos.size() >= 1) {
+                                int antepenultimate = tempLpos.get(tempLpos.size() - 1);
+                                lastPos.add(antepenultimate);
+                            }                            
+                        }
                     } 
 
                     else if (stack2.size() >= 2 && !stack2.get(stack2.size() - 1) && !stack2.get(stack2.size() - 2)) {
@@ -78,8 +112,11 @@ public class RegexToDFA {
                         recurBooleanC1 = false; 
                         recurBooleanC2 = false;  
                         
-                        nullable = true;
+                        nullable = false;
                         stack2.add(nullable);
+
+                        Integer item1 = tempFpos.peek();
+                        firstPos.add(item1);
                     }
 
                     else {
@@ -88,11 +125,29 @@ public class RegexToDFA {
                         recurBooleanC2 = stack2.get(stack2.size() - 1); 
                         
                         nullable = false;
-                        stack2.add(nullable);
-                    }
+                        stack2.add(nullable); 
+                        
+                        // currNode = stack3.pop();
+                        // aNode = stack3.pop();
+                        // bNode = stack3.pop();
 
-                    aNode = stack3.pop();
-                    bNode = stack3.pop();
+                        if(recurBooleanC1 == true){
+                            int count = 0;
+                            while(!tempFpos.isEmpty() && count < 2){
+                                Integer item = tempFpos.pop();
+                                firstPos.add(item);
+                                count++;
+                            }
+                        }
+
+                        else{
+                            if (tempFpos.size() >= 3) {
+                                int antepenultimate = stack.get(stack.size() - 3);
+                                tempFpos.add(antepenultimate);
+                            }                            
+                        }
+                    }
+                    
                     break;
 
                 case '*':
@@ -100,8 +155,12 @@ public class RegexToDFA {
                     nullable = true;
                     stack2.add(nullable);
 
-                    aNode = stack3.pop();
-                    bNode = stack3.pop();
+                    // currNode = stack3.pop();
+                    // aNode = stack3.pop();
+                    // bNode = stack3.pop();
+
+                    firstPos.addAll(tempFpos);
+                    lastPos.addAll(tempLpos);
                     break;
 
                 case '|':
@@ -109,11 +168,7 @@ public class RegexToDFA {
                     // nullable = false;
                     // stack2.push(nullable);
 
-                    firstPos = stack;
-                    lastPos = stack;
-
-                    aNode = stack3.pop();
-                    bNode = stack3.pop();
+                    //if true, is nullable
 
                     if (stack2.size() >= 2 && stack2.get(stack2.size() - 1) && stack2.get(stack2.size() - 2)) {
                         //Both are true
@@ -130,7 +185,7 @@ public class RegexToDFA {
                         recurBooleanC1 = false; 
                         recurBooleanC2 = false; 
 
-                        nullable = true;
+                        nullable = false;
                         stack2.add(nullable);
                     }
                     
@@ -144,6 +199,22 @@ public class RegexToDFA {
                         
                     }
 
+
+                    //Getting the last 2 elements in the stack (C1 and C2)
+                    int count = 0;
+                    while(!tempFpos.isEmpty() && count < 2){
+                        Integer item = tempFpos.get(tempFpos.size());
+                        firstPos.add(item);
+                        count++;
+                    }
+
+                    int count2 = 0;
+                    while(!tempLpos.isEmpty() && count2 < 2){
+                        Integer item = tempLpos.get(tempLpos.size());
+                        lastPos.add(item);
+                        count2++;
+                    }
+
                     break;
 
                 case 'ε':
@@ -154,11 +225,17 @@ public class RegexToDFA {
 
                 default:
                     //It's NOT nullable
-                    nullable = false;
+                    nullable = false; //currNode
                     counter++;
                     
-                    firstPos.add(counter);
-                    lastPos.add(counter);
+                    tempFpos.add(counter);
+                    tempLpos.add(counter);
+                    // firstPos.add(counter);
+                    // lastPos.add(counter); 
+                    /* 
+                     * tempFpos = [1,2]  fPos = [1,2]
+                     * tempFpos = [1,2,3] 
+                     */
                     
                     stack.add(counter);
                     stack2.add(nullable);
@@ -168,16 +245,16 @@ public class RegexToDFA {
         }
     }
 
-    public void setMatrix(int[][] matrix) {
+    /* public void setMatrix(int[][] matrix) {
         this.matrix = matrix;
-    }
+    } */
 
-    public int[][] getMatrix() {
+    /* public int[][] getMatrix() {
         return matrix;
-    }
+    } */
 
 
-    public void getTreeLeafs(Stack<String> st){
+/*     public void getTreeLeafs(Stack<String> st){
         Iterator<String> iterator = st.iterator();
         String aNode = "";
         String bNode = "";
@@ -188,6 +265,6 @@ public class RegexToDFA {
 
         }
     }
-    
+     */
     
 }
